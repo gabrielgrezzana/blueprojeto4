1745d9c1e14b3ef86c311a54865e1bb941cfbb49
gabrielgrezzana <106358234+gabrielgrezzana@users.noreply.github.com>
2022-08-30 19:03:23 -0300

    atualiza

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const connectToDatabase = require('./src/database/database');
-
 const authRoutes = require('./src/auth/auth.routes');
 const charactersRoutes = require('./src/characters/characters.routes');
 const usersRoutes = require('./src/users/users.routes');
-
+const connectToDatabase = require('./src/database/database');
 connectToDatabase();
 app.use(cors());
 app.use(express.json());
 
 const swagger = require('swagger-ui-express');
+
 const swaggerDocs = require('./src/docs/swagger.json');
+
 app.use('/docs', swagger.serve, swagger.setup(swaggerDocs));
 
 app.use('/auth', authRoutes);
