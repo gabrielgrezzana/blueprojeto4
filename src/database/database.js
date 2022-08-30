438eadb6a24e8ec04ad369a7869e907c2014ba38
gabrielgrezzana <106358234+gabrielgrezzana@users.noreply.github.com>
2022-08-30 19:31:36 -0300

    atualiza

--- src/database/database.js
+++ src/database/database.js
@@ -1,11 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 module.exports = connectToDatabase = () => {
   mongoose
-    .connect(process.env.MONGO_URI, {
+    .connect(String(process.env.MONGO_URI), {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
-    .then(() => console.log("Connect to Database"))
+    .then(() => console.log('Connect to Database'))
     .catch((err) => console.error(`Error connecting to Database: ${err}`));
 };
