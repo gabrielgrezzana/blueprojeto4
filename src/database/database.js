1740090668c98596d84c9668facc63bbfad5980f
gabrielgrezzana <106358234+gabrielgrezzana@users.noreply.github.com>
2022-08-30 19:39:43 -0300

    atulz

--- src/database/database.js
+++ src/database/database.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
-module.exports = connectToDatabase = () => {
+const connectToDatabase = () => {
+  console.log('Conectando com o MongoDB...');
   mongoose
     .connect(String(process.env.MONGO_URI), {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
-    .then(() => console.log('Connect to Database'))
-    .catch((err) => console.error(`Error connecting to Database: ${err}`));
+    .then(() => console.log('MongoDB conectado com sucesso!'))
+    .catch((error) => {
+      console.log(`Erro ao conectar com o MongoDB: ${error}`);
+    });
 };
+
+module.exports = connectToDatabase;
